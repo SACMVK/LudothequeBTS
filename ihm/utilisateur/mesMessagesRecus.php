613b71f6cd37032b74eb31c7fe69fba37d36710e
charlotteDavid <charlotte.david96@gmail.com>
2017-05-23 15:37:32 +0200

    message

--- ihm/utilisateur/mesMessagesRecus.php
+++ ihm/utilisateur/mesMessagesRecus.php
@@ -18,6 +18,12 @@
                         <input type=hidden name="actionToDoWithObject" value="read" />
                                                
                     </form>
+                    <form action=" " method="post" accept-charset="utf-8">
+                        <input type=hidden name="objectToWorkWith" value="Message" />
+                        <input type=hidden name="actionToDoWithObject" value="delete" />
+                        <input type="hidden" name="idMessage" value="<?= $message->getIdmessage() ?>" />
+                        <input type="image" name="submit" class="boutonTransparent" value="Supprimer ce message de ma ludothèque" src="ihm/img/delete.png">
+                    </form>
                 </td>
             </tr> <br>
             <?php
